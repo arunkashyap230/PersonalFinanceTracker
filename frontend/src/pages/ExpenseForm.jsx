2727193b2461228ab4fddf371b00f299d104123e
Arun kashyap <70384465+arunkashyap230@users.noreply.github.com>
2025-07-10 13:04:02 +0530

    Update ExpenseForm.jsx

--- frontend/src/pages/ExpenseForm.jsx
+++ frontend/src/pages/ExpenseForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./ExpenseForm.css";
 
 function ExpenseForm({ expenseToEdit, onSuccess }) {
   const [form, setForm] = useState({
