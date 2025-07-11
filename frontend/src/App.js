138892349edeb74d2639b14e872f8feeb1ad03cc
Arun kashyap <70384465+arunkashyap230@users.noreply.github.com>
2025-07-11 17:03:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,21 @@ import Dashboard from "./pages/Dashboard";
 import ExpenseForm from "./pages/ExpenseForm";
 import ExpensesList from "./pages/ExpensesList";
 import ReportsPage from "./pages/ReportsPage";
+import PublicExpensesPage from "./pages/PublicExpensesPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/" element={<PublicExpensesPage />} />{" "}
+      {/* 👈 Set as default page */}
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/add-expense" element={<ExpenseForm />} />
       <Route path="/expenses" element={<ExpensesList />} />
       <Route path="/reports" element={<ReportsPage />} />
+      <Route path="/public-expenses" element={<PublicExpensesPage />} />
     </Routes>
   );
 }
